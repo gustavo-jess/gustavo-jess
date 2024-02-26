755569cdc86ee818a05db77115a17b1b2d987f90
gustavo-jess <160682673+gustavo-jess@users.noreply.github.com>
2024-02-25 22:10:38 -0300

    AT1.java

---
+++ AT1.java
@@ -0,0 +1,14 @@
+classe  Olá Mundo {
+    public  static  void  main ( String [] args ) {
+         Nome da string ;
+         valor flutuante = 1,2f ;
+        int  idade = 90 ;
+        Inteiro  ano = 20 ;
+         peso duplo = 20,5 ;
+        booleano  confirma = true ;
+        char  sexo = 'M' ;
+         numG longo = 374636404 ;
+        byte  numP = 2584
+         número curtoC = 321
+    }
+}
