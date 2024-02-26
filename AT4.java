b904329d99c586b5aa94e70ce716f285b580f4af
gustavo-jess <160682673+gustavo-jess@users.noreply.github.com>
2024-02-25 22:16:55 -0300

    AT4.java

---
+++ AT4.java
@@ -0,0 +1,9 @@
+public class ControleEstoque {
+    public static void main(String[] args) {
+        String nomeProd;
+        String categoriaProd
+        double precoUnitario;
+        int qtdEmProduto
+
+    }
+}
