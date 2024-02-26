99ffc42d28e4f0133f54b51b78bbda6e0b76a207
gustavo-jess <160682673+gustavo-jess@users.noreply.github.com>
2024-02-25 22:19:29 -0300

    AT6.java

---
+++ AT6.java
@@ -0,0 +1,18 @@
+ classe  pública Principal {
+    public  static  void  main ( String [] args ) {
+
+        int  numeroProduto = 321 ;
+        double  precoProduto = 55 ;
+
+        String  descriçãoProduto ;
+        descricaoProduto = "Fone de ouvido Bluetooth" ;
+
+        Sistema . fora . println ( "Descrição do Produto: " + descriçãoProduto );
+
+
+        Sistema . fora . println ( "Dados do Produto:" );
+        Sistema . fora . println ( "Número: " + numeroProduto );
+        Sistema . fora . println ( "Preço: $" + precoProduto );
+        Sistema . fora . println ( "Descrição: " + descriçãoProduto );
+    }
+}
