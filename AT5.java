4273b746778af098279c9d6db569cd4cfbdedf3b
gustavo-jess <160682673+gustavo-jess@users.noreply.github.com>
2024-02-25 22:18:16 -0300

    AT5.java

---
+++ AT5.java
@@ -0,0 +1,43 @@
+  classe  pública AT5 {
+    public  static  void  main ( String [] args ) {
+
+        String  nomeSmartphone = "Redmi note 8" ;
+        String  marcaSmartphone = "Xaomi" ;
+         preço duploSmartphone = 1200,00 ;
+        int  qtdSmartphone = 50 ;
+
+        Sistema . fora . println ( "Dados do Smartphone:" );
+        Sistema . fora . println ( "Nome: " + nomeSmartphone );
+        Sistema . fora . println ( "Marca: " + marcaSmartphone );
+        Sistema . fora . println ( "Preço: " + precoSmartphone );
+        Sistema . fora . println ( "Estoque: " + qtdSmartphone );
+
+       Sistema . fora . println ( " " );
+
+        String  nomeLaptop = "G15" ;
+        String  marcaLaptop = "Dell" ;
+        duplo  precoLaptop = 5000,00 ;
+        int  qtdLaptop = 10 ;
+
+        Sistema . fora . println ( "Dados do Laptop:" );
+        Sistema . fora . println ( "Nome: " + nomeLaptop );
+        Sistema . fora . println ( "Marca: " + marcaLaptop );
+        Sistema . fora . println ( "Preço: " + precoLaptop );
+        Sistema . fora . println ( "Estoque: " + qtdLaptop );
+
+        Sistema . fora . println ( " " );
+
+        String  nomeFoneDeOuvido = "AirPods" ;
+        String  marcaFoneDeOuvido = "Apple" ;
+        duplo  precoFoneDeOuvido = 1100,00 ;
+        int  qtdFoneDeOuvido = 40 ;
+
+        Sistema . fora . println ( "Dados do Fone De Ouvido:" );
+        Sistema . fora . println ( "Nome: " + nomeFoneDeOuvido );
+        Sistema . fora . println ( "Marca: " + marcaFoneDeOuvido );
+        Sistema . fora . println ( "Preço: " + precoFoneDeOuvido );
+        Sistema . fora . println ( "Estoque: " + qtdFoneDeOuvido );
+
+
+    }
+}
