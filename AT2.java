2760628e7492436b21929c15af8d5e6b4292c627
gustavo-jess <160682673+gustavo-jess@users.noreply.github.com>
2024-02-25 22:13:35 -0300

    AT2.java

---
+++ AT2.java
@@ -0,0 +1,14 @@
+classe  AT2 {
+    public  static  void  main ( String [] args ) {
+        String  nomeProd ;
+        duplo  precoProd ;
+        String  marcaDoProd ;
+        int  quantidadeProduto ;
+        String  descriçãoProduto ;
+        String  nomeDoProd ;
+        int  numeroProd ;
+        double  precoDoProd ;
+        int  estoqueDisp ;
+        int  totalDeProd ;
+    }
+}
