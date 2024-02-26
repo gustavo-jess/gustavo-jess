c8b6b8bd9d9dc5ac799425e7a94c16685dae9632
gustavo-jess <160682673+gustavo-jess@users.noreply.github.com>
2024-02-25 22:20:55 -0300

    AT7.java

---
+++ AT7.java
@@ -0,0 +1,20 @@
+public  class  ControleEstoque {
+    public  static  void  main ( String [] args ) {
+
+        String  marcaProduto ;
+        String  nomeProduto ;
+        duplo  precoProduto ;
+        int  quantidadeProduto ;
+
+        marcaProduto = "apple" ;
+        nomeProduto = "apple whatch" ;
+        precoProduto = 19,99 ;
+        quantidadeProduto = 16 ;
+
+        Sistema . fora . println ( "Dados do Produto: " );
+        Sistema . fora . println ( "Nome: " + nomeProduto );
+        Sistema . fora . println ( "Marca: " + marcaProduto );
+        Sistema . fora . println ( "Preço: $" + precoProduto );
+        Sistema . fora . println ( "Quantidade em estoque: " + quantidadeProduto );
+    }
+}
